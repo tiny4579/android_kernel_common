ce9f1439963f958a201ec53d01ff5c50e8b68e86
Chad Froebel <chad0989@rochester.rr.com>
2011-02-27 12:34:16 -0500

    3/5 point multi-touch

--- drivers/input/touchscreen/atmel.c
+++ drivers/input/touchscreen/atmel.c
@@ -1148,6 +1148,22 @@ static int atmel_ts_probe(struct i2c_client *client,
 		ts->id->matrix_x_size, ts->id->matrix_y_size,
 		ts->id->num_declared_objects);
 
+        /*
+        Begin ffolkes multitouch selection
+        Purpose: Some hardware can't support more than 3 multitouch points, so revert back to 3 if we find a 004F touch sensor
+        BEGIN
+        */
+        if(ts->id->family_id == 0x4F && pdata->config_T9[14] != 3) {
+                printk(KERN_INFO "3 point multitouch enabled\n");
+                pdata->config_T9[14] = 3;
+        } else {
+                printk(KERN_INFO "5 point multitouch enabled - NOTE: Some devices may revert to 2 point\n");
+                pdata->config_T9[14] = 5;
+        }
+        /*
+        END
+        */
+
 	/* Read object table. */
 	ret = read_object_table(ts);
 	if (ret < 0)
