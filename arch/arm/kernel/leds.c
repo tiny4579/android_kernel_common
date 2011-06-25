351fc5480d0d7de37044a86ee2396ae9043174d0
Todd Poynor <toddpoynor@google.com>
2011-06-24 19:33:01 -0700

    ARM: Move leds idle start/stop calls to idle notifiers
    Change-Id: I5d8e4e85b17bbab7992ecb477f0bdb5e4138b166
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- arch/arm/kernel/leds.c
+++ arch/arm/kernel/leds.c
@@ -9,6 +9,8 @@
  */
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/notifier.h>
+#include <linux/cpu.h>
 #include <linux/sysdev.h>
 
 #include <asm/leds.h>
@@ -97,6 +99,24 @@ static struct sysdev_class leds_sysclass = {
 static struct sys_device leds_device = {
 	.id		= 0,
 	.cls		= &leds_sysclass,
+
+static int leds_idle_notifier(struct notifier_block *nb, unsigned long val,
+                                void *data)
+{
+	switch (val) {
+	case IDLE_START:
+		leds_event(led_idle_start);
+		break;
+	case IDLE_END:
+		leds_event(led_idle_end);
+		break;
+	}
+
+	return 0;
+}
+
+static struct notifier_block leds_idle_nb = {
+	.notifier_call = leds_idle_notifier,
 };
 
 static int __init leds_init(void)
@@ -107,6 +127,12 @@ static int __init leds_init(void)
 		ret = sysdev_register(&leds_device);
 	if (ret == 0)
 		ret = sysdev_create_file(&leds_device, &attr_event);
+
+	if (ret == 0) {
+		register_syscore_ops(&leds_syscore_ops);
+		idle_notifier_register(&leds_idle_nb);
+	}
+
 	return ret;
 }
 
