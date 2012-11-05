e760a221a3ac230bf92e3cf7734dc30ded841ada
Corey Moyer <corey@CronBox4-Dev.(none)>
2012-11-04 23:38:47 -0500

    Fix EXT4 should writeback data for non journal mode

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -1846,6 +1846,8 @@ static int ext4_journalled_write_end(struct file *file,
 	from = pos & (PAGE_CACHE_SIZE - 1);
 	to = from + len;
 
+	BUG_ON(!ext4_handle_valid(handle));
+
 	if (copied < len) {
 		if (!PageUptodate(page))
 			copied = 0;
@@ -2658,6 +2660,8 @@ static int __ext4_journalled_writepage(struct page *page,
 		ret = PTR_ERR(handle);
 		goto out;
 	}
+
+	BUG_ON(!ext4_handle_valid(handle));
 
 	ret = walk_page_buffers(handle, page_bufs, 0, len, NULL,
 				do_journal_get_write_access);
