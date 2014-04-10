6998c627402273d62a1b60aa74f1f29b543db5e5
Zach <zachf714@gmail.com>
2014-04-10 17:51:05 -0400

    Fix BFS

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -243,7 +243,7 @@ void mmc_wait_for_req(struct mmc_host *host, struct mmc_request *mrq)
 		msmsdcc_request_end(msm_host, mrq); 	
 	}
 #else
-	wait_for_completion_io(&complete);
+	wait_for_completion(&complete);
 #endif		
 
 }
